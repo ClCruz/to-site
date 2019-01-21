38c1f9ac0676236893d1040e44ff52521e01f250
Gabriel Craveiro <gabriel.craveiro@intuiti.com.br>
2019-01-21 12:19:55 -0200

    amigosecia eventoslitoral

--- src/views/templates/ticketHubVideoBackground/Events.vue
+++ src/views/templates/ticketHubVideoBackground/Events.vue
@@ -1,8 +1,8 @@
 <template>
 <div class="a">
-  <section class="to-block to-viewport bg-dark bg__main" style="position: absolute; top: 0; width: 99vw; z-index: -1" data-block-type="call_to_action" data-id="2">
+  <section class="to-block to-viewport bg-dark bg__main" style="position: absolute; top: 0; width: 99vw; z-index: -1;background: none!important" data-block-type="call_to_action" data-id="2">
     <video autoplay muted loop id="myVideo" style="max-height: 92vh; object-fit: cover; width: 100vw; overflow-x: hidden;">
-      <source src="/assets/teste2.mp4" type="video/mp4">
+      <source src="/assets/noabeachclub.mp4" type="video/mp4">
     </video>
   </section>
 
