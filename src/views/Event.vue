aff121d1c9e2046adc4d51195ee5f4e05ed15204
blcoccaro <blcoccaro@gmail.com>
2019-02-11 09:59:45 -0200

    removed ticketoffice

--- src/views/Event.vue
+++ src/views/Event.vue
@@ -548,6 +548,8 @@ export default {
 
           if (response.error) {
             this.toastError(response.msg);
+            if (response.goto == "home")
+              window.location = "/";
             return;
           }
           if (this.validateJSON(response)) {
