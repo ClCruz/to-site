c9c4d2b03384b94deae76eff44ed491f70dc3660
Gabriel Craveiro <gabriel.craveiro@intuiti.com.br>
2019-02-20 09:51:58 -0300

    Inclusão de jsconfig no gitignore

---
+++ src/components/loaders/NextEventsLoader.vue
@@ -0,0 +1,24 @@
+<template>
+	<content-loader
+		:height="80"
+		:width="180"
+		:speed="2"
+		primaryColor="#f3f3f3"
+		secondaryColor="#ecebeb"
+	>
+		<rect x="0" y="0" rx="10" ry="5" width="180" height="80" />
+	</content-loader>
+</template>
+
+<script>
+import {
+  ContentLoader
+} from "vue-content-loader"
+
+export default {
+  name: "NextEventsLoader",
+  components: {
+    ContentLoader
+  }
+}
+</script>
