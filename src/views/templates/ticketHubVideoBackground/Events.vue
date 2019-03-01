cc594d10d5ac6bd9c2910ded3d5bc33f6c7ba4b0
Gabriel Craveiro <gabriel.craveiro@intuiti.com.br>
2019-03-01 17:18:54 -0300

    Generos para uppercase

--- src/views/templates/ticketHubVideoBackground/Events.vue
+++ src/views/templates/ticketHubVideoBackground/Events.vue
@@ -237,7 +237,7 @@ export default {
     removeDuplicatesBy(keyFn, array) {
       var mySet = new Set();
       return array.filter(function (x) {
-        var key = keyFn(x),
+        var key = keyFn(x).toUpperCase(),
           isNew = !mySet.has(key);
         if (isNew) mySet.add(key);
         return isNew;
