a3e1dae7d7983a4129aeda811646bdbe10c425b6
Gabriel Craveiro <gabriel.c.craveiro@gmail.com>
2019-04-03 11:32:19 -0300

    Criação do WL ingressaria

--- gulpfile.js
+++ gulpfile.js
@@ -36,7 +36,7 @@ gulp.task('sass', function() {
 
 gulp.task('sassLocal', function() {
 	gulp
-		.src('./src/scss/themes/www.ingresso*/*.scss')
+		.src('./src/scss/themes/www.ingress*/*.scss')
 		.pipe(
 			sass({
 				outputStyle: 'nested'
