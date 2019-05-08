a278e665bba47f57537c6d654b357d9f66fa0af9
blcoccaro <blcoccaro@gmail.com>
2019-05-08 17:26:14 -0300

    tentativa de tirar o cache numero 85458

--- src/sw.js
+++ src/sw.js
@@ -1,23 +1,52 @@
 // This is the code piece that GenerateSW mode can't provide for us.
 // This code listens for the user's confirmation to update the app.
 self.addEventListener('message', (e) => {
-    if (!e.data) {
-      return;
-    }
-  
-    switch (e.data) {
-      case 'skipWaiting':
-        self.skipWaiting();
-        break;
-      default:
-        // NOOP
-        break;
-    }
-  });
-  
-  workbox.clientsClaim();
-  
-  // The precaching code provided by Workbox.
-  self.__precacheManifest = [].concat(self.__precacheManifest || []);
-  workbox.precaching.suppressWarnings();
-  workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+  if (!e.data) {
+    return;
+  }
+
+  switch (e.data) {
+    case 'skipWaiting':
+      self.skipWaiting();
+      break;
+    default:
+      // NOOP
+      break;
+  }
+});
+
+workbox.clientsClaim();
+
+// workbox.routing.registerRoute(
+//   new workbox.strategies.NetworkFirst()
+// );
+
+self.addEventListener('install', event => {
+  console.log('V2 installing…');
+
+  // cache a horse SVG into a new cache, static-v2
+  event.waitUntil(
+    caches.open('static-v2').then(cache => cache.add('/horse.svg'))
+  );
+});
+
+self.addEventListener('activate', event => {
+  // delete any caches that aren't in expectedCaches
+  // which will get rid of static-v1
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.map(key => {
+        if (!expectedCaches.includes(key)) {
+          return caches.delete(key);
+        }
+      })
+    )).then(() => {
+      console.log('V2 now ready to handle fetches!');
+    })
+  );
+});
+
+// The precaching code provided by Workbox.
+//self.__precacheManifest = [].concat(self.__precacheManifest || []);
+//workbox.precaching.suppressWarnings();
+//workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
