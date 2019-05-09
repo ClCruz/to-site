08bb2049c5c6995c4bc84a7a51df73449a304d0f
Gabriel Craveiro <gabriel.c.craveiro@gmail.com>
2019-05-09 11:18:32 -0300

    Alterações IT

--- gulpfile.js
+++ gulpfile.js
@@ -36,7 +36,7 @@ gulp.task('sass', function() {
 
 gulp.task('sassLocal', function() {
 	gulp
-		.src('./src/scss/themes/*promo*/*.scss')
+		.src('./src/scss/themes/*paratodos*/*.scss')
 		.pipe(
 			sass({
 				outputStyle: 'nested'
