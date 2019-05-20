6d421762a8b51d91f9646acdb6fc8733fff4d02c
Gabriel Craveiro <gabriel.c.craveiro@gmail.com>
2019-05-20 13:59:09 -0300

    Alteração no cache (datestamp)

--- src/sw.js
+++ src/sw.js
@@ -15,9 +15,9 @@ self.addEventListener('message', (e) => {
     }
   });
   
-  workbox.clientsClaim();
+  // workbox.clientsClaim();
   
   // The precaching code provided by Workbox.
-  self.__precacheManifest = [].concat(self.__precacheManifest || []);
-  workbox.precaching.suppressWarnings();
-  workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+  // self.__precacheManifest = [].concat(self.__precacheManifest || []);
+  // workbox.precaching.suppressWarnings();
+  // workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
