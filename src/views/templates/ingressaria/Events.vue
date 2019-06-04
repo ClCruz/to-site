a841e9401911eac876c4eae3774e2bf2f4ea8cc6
Gabriel Craveiro <gabriel.c.craveiro@gmail.com>
2019-06-04 14:01:35 -0300

    Alteração no marketplay.club

--- src/views/templates/ingressaria/Events.vue
+++ src/views/templates/ingressaria/Events.vue
@@ -8,7 +8,7 @@
       </div>
       <div class="col-12 col-md-10 justify-content-center text-center" v-if="siteName != 'ingressoparatodos.com.br'">
         <div class="" style="">
-          <h1>Pesquise experiências</h1>
+          <h1>Procure eventos</h1>
         </div>
         <div class="input-group mt-0 mb-3 p-2 w-100">
           <app-search></app-search>
@@ -18,7 +18,7 @@
     <div class="container justify-content-center align-items-center d-flex p-4  pt-5 container__search--tickethub" v-else>
       <div class="col-12 col-md-10 justify-content-center text-center" v-if="siteName != 'ingressoparatodos.com.br'">
         <div class="" style="">
-          <h1>Pesquise experiências</h1>
+          <h2>Procure por experiências únicas.</h2>
         </div>
         <div class="input-group mt-0 mb-3 p-2 w-100">
           <app-search></app-search>
