46e1603be84df73299205743be2695f066fc6cba
Gabriel Craveiro <gabriel.c.craveiro@gmail.com>
2019-07-23 10:24:07 -0300

    Oqueteinspira

--- gulpfile.js
+++ gulpfile.js
@@ -36,7 +36,7 @@ gulp.task('sass', function() {
 
 gulp.task('sassLocal', function() {
 	gulp
-		.src('./src/scss/themes/*compre*/*.scss')
+		.src('./src/scss/themes/*revival*/*.scss')
 		.pipe(
 			sass({
 				outputStyle: 'nested'
