098fea67cfb6996405d540645dbe606210c24b0d
Gabriel Craveiro <gabriel.c.craveiro@gmail.com>
2019-08-08 10:37:50 -0300

    WL Demo

--- gulpfile.js
+++ gulpfile.js
@@ -36,7 +36,7 @@ gulp.task('sass', function() {
 
 gulp.task('sassLocal', function() {
 	gulp
-		.src('./src/scss/themes/*compre*/*.scss')
+		.src('./src/scss/themes/*demo*/*.scss')
 		.pipe(
 			sass({
 				outputStyle: 'nested'
